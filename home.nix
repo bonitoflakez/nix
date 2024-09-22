3e7b1e07f76d7d54c559e2c744a8a83a98b58540
Nikhil <nikhildhiman9050@gmail.com>
2024-09-22 18:16:04 +0530

    home packages, zsh and git config

--- home.nix
+++ home.nix
@@ -17,9 +17,71 @@ in
   programs.git = {
     enable = true;
     aliases = {
-      maingit = "!f() { git config user.name \"${private.people.primary.name}\" && git config user.email \"${private.people.primary.mail}\"; }; f";
-      kysgit = "!f() { git config user.name \"${private.people.secondary.name}\" && git config user.email \"${private.people.secondary.mail}\"; }; f";
+      creds = ''
+        !f() {
+          git config user.name && git config user.email
+        }; f
+      '';
+      maingit = ''
+        !f() {
+          git config user.name "$PRIMARYNAME" && git config user.email "$PRIMARYMAIL";
+        }; f
+      '';
+      kysgit = ''
+        !f() {
+          git config user.name "$SECONDARYNAME" && git config user.email "$SECONDARYMAIL";
+        }; f
+      '';
     };
+  };
+
+  programs.zsh = {
+    enable = true;
+    sessionVariables = {
+      DONE = "yes.";
+    };
+    initExtra = ''
+      # If you come from bash you might have to change your $PATH.
+      export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$HOME/.config/emacs/bin:$PATH
+
+      # Path to your Oh My Zsh installation.
+      export ZSH="$HOME/.oh-my-zsh"
+
+      # import gitconfig
+      if [ -f ~/home-manager/.gitprivate ]; then
+         source ~/home-manager/.gitprivate
+      fi
+
+      ZSH_THEME="robbyrussell"
+
+      plugins=(
+        git
+        zsh-autosuggestions
+        fast-syntax-highlighting
+                )
+
+      source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+      source $ZSH/oh-my-zsh.sh
+
+      # export MANPATH="/usr/local/man:$MANPATH"
+
+      # You may need to manually set your language environment
+      # export LANG=en_US.UTF-8
+
+      # Preferred editor for local and remote sessions
+      # if [[ -n $SSH_CONNECTION ]]; then
+      #   export EDITOR='vim'
+      # else
+      #   export EDITOR='nvim'
+      # fi
+
+      # Compilation flags
+      # export ARCHFLAGS="-arch $(uname -m)"
+
+      # Example aliases
+      # alias zshconfig="mate ~/.zshrc"
+      # alias ohmyzsh="mate ~/.oh-my-zsh"
+    '';
   };
 
   home = {
