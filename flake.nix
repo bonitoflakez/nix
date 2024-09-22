f2e42d5961bb3fa37636184f3027093e59af6cfe
Nikhil <nikhildhiman9050@gmail.com>
2024-09-22 11:24:07 +0530

    first home-manager commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "home manager config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations = {
+        syk = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, ... };
+
+{
+  home = {
+    packages = with pkgs; [
+      hello
+    ];
+
+    username = "syk";
+    homeDirectory = "/home/syk";
+  
+    stateVersion = "23.11";
+  };
+}
+
